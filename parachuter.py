f45c5fff57aefda2665f657a4f8976087edff60d
andresperez1994 <97799396+andresperez1994@users.noreply.github.com>
2022-02-11 00:16:14 -0500

    Create parachuter.py

---
+++ parachuter.py
@@ -0,0 +1,64 @@
+class Parachuter: 
+    def __init__(self):
+        self.mistakes = 0
+    
+    def draw_0 (self):
+            print("      _____   ")
+            print("    / _____ \ ")
+            print("    \       / ")
+            print("     \     / ")
+            print("       😀    ")
+            print("      / | \  ")
+            print("       / \  ")
+            print()
+            print("   ^^^^^^^^^^  ")
+    def draw_1(self):    
+            
+            print()
+            print("    / ____  \ ")
+            print("    \       / ")
+            print("     \     / ")
+            print("       😕    ")
+            print("      / | \  ")
+            print("       / \  ")
+            print()
+            print("   ^^^^^^^^^^  ")
+    
+    def draw_2(self):        
+        
+            print()
+            print("             ")
+            print("    \       / ")
+            print("     \     / ")
+            print("       😬    ")
+            print("      / | \  ")
+            print("       / \  ")
+            print()
+            print("   ^^^^^^^^^^  ")
+        
+    def draw_3(self):    
+            print()
+            print("             ")
+            print("             ")
+            print("     \     / ")
+            print("       😩   ")
+            print("      / | \  ")
+            print("       / \  ")
+            print()
+            print("   ^^^^^^^^^^  ")
+            
+    def draw_4(self): 
+            print()
+            print("             ")
+            print("             ")
+            print("             ")
+            print("       💀    ")
+            print("      / | \  ")
+            print("       / \  ")
+            print()
+            print("   ^^^^^^^^^^  ")
+            
+
+
+     
+         
